f7d5efc7e900852055c2a51d63af8690187cd641
Pablo Lorenzo <plorenzo@gmail.com>
2018-11-03 16:01:51 +0000

    Rover MoveForward different directions

--- PlutoRover.Unit.Tests/RoverTests.cs
+++ PlutoRover.Unit.Tests/RoverTests.cs
@@ -14,12 +14,16 @@ namespace PlutoRover.Unit.Tests
         }
 
         [Test]
-        public void Rover_WhenMoveForwardIsCalled_ThenUpdatePosition()
+        [TestCase(0, 0, Orientation.N, "0,1,N")]
+        [TestCase(50, 50, Orientation.E, "51,50,E")]
+        [TestCase(50, 50, Orientation.S, "50,49,S")]
+        [TestCase(50, 50, Orientation.W, "49,50,W")]
+        public void Rover_WhenMoveForwardIsCalled_ThenUpdatePosition(int positionX, int positionY, Orientation orientation, string result)
         {
-            var initialPosition = new Position{x = 0, y = 0, Orientation = Orientation.N};
+            var initialPosition = new Position{x = positionX, y = positionY, Orientation = orientation};
             var rover = new Rover(initialPosition);
             rover.MoveForward();
-            rover.PositionReported.Should().Be("0,1,N");
+            rover.PositionReported.Should().Be(result);
         }
     }
 }
